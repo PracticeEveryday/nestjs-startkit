6c4e02619c58ef61f18a470abb5ac6f66bd6de7f
PracticeEveryday <developerforus@gmail.com>
2023-12-26 10:04:40 +0900

    feat: swagger setting

---
+++ src/libs/swagger/swagger.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
+
+export function setupSwagger(app: INestApplication): void {
+  const SWAGGER_USER = process.env['SWAGGER_USER'] as string;
+
+  app.use(
+    ['/api'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [SWAGGER_USER]: process.env['SWAGGER_PASSWORD'] as string,
+      },
+    }),
+  );
+
+  const swaggerInfo = fs.readFileSync(
+    'src/libs/swagger/swagger-info.md',
+    'utf-8',
+  );
+
+  const options = new DocumentBuilder()
+    .setTitle('Nestjs-startkit')
+    .setDescription(swaggerInfo)
+    .setVersion('0.0.1')
+    .addServer(`http://localhost:${process.env.PORT}/api`, '로컬서버')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { EnvService } from './libs/env/env.service';
 import { EnvEnum } from './libs/env/env.enum';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston/dist/winston.constants';
+import { setupSwagger } from './libs/swagger/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const envService = app.get(EnvService);
   const PORT = +envService.get<EnvEnum>(EnvEnum.PORT) || 3000;
 
+  setupSwagger(app);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   await app.listen(PORT);
